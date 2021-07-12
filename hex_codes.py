5e59c6ab5e8fdef7a0865e2132c7ff5e136a766d
Luis Antonio Haddock III <55507927+haddocksoto@users.noreply.github.com>
2021-07-12 12:05:56 -0500

    Update hex_codes.py

--- hex_codes.py
+++ hex_codes.py
@@ -1,4 +1,7 @@
 #generating n number of distinct hex codes
+
+import random
+
 number_of_colors = 3000
 color = ["#" + ''.join([random.choice('0123456789ABCDEF') for x in range(6)])
              for c in range(number_of_barcodes)]
