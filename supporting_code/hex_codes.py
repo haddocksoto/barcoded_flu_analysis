cfd19cac998200bcbdf4b606154ec7565e2ed673
Luis Antonio Haddock III <55507927+haddocksoto@users.noreply.github.com>
2021-08-05 16:16:29 -0500

    Update hex_codes.py

--- supporting_code/hex_codes.py
+++ supporting_code/hex_codes.py
@@ -1,7 +1,9 @@
-#generating n number of distinct hex codes
+#generating n number of distinct hex codes for our barcodes
 
 import random
 
-number_of_colors = 3000
+number_of_colors = 3000 #change value as desired
 color = ["#" + ''.join([random.choice('0123456789ABCDEF') for x in range(6)])
              for c in range(number_of_barcodes)]
+
+#output: list of hex codes
