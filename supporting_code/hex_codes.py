4879c88e38b9014e6cbe19b2534aaf25cf6c7d0d
Luis Antonio Haddock III <55507927+haddocksoto@users.noreply.github.com>
2021-08-19 18:49:06 -0500

    Hex code generator
    Creates n number of hex codes for input.

--- supporting_code/hex_codes.py
+++ supporting_code/hex_codes.py
@@ -6,4 +6,4 @@ number_of_colors = 3000 #change value as desired
 color = ["#" + ''.join([random.choice('0123456789ABCDEF') for x in range(6)])
              for c in range(number_of_barcodes)]
 
-#output: list of hex codes
+#output: list of hex codes 
